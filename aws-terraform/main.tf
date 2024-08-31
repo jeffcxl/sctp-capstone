82d13623f2310657535eeae08357918155dec15c
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2024-08-31 15:43:21 +0800

    created vpc and subnets using terraform

---
+++ aws-terraform/main.tf
@@ -0,0 +1,31 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.environment}-public-${var.availability_zones[count.index]}"
+  }
+}
+
+# Create Private Subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "${var.environment}-private-${var.availability_zones[count.index]}"
+  }
+}
--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce6-tfstate"
-    key    = "capstone-atuo-report.tfstate" #Remember to change this
-    region = "ap-southeast-1"      #The region of your backend bucket
-  }
-}
